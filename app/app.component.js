5c5e15fe82fcfea654d9ce58053c6d176a2862c6
Pete <peter.s.giles@gmail.com>
2016-07-08 12:54:22 +1000

    quick start done

--- app/app.component.js
+++ app/app.component.js
@@ -9,14 +9,21 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var hero_service_1 = require('./hero.service');
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Converty';
+        this.title = 'Tour of Heroes';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1>{{title}}</h1>\n\n  <article>\n      <input type=\"file\" id=\"imageInput\" accept=\"image/*\">\n      <div id=\"imgdiv\"> </div>\n      <div> </div>\n      <div id=\"svgdiv\"> </div>\n  </article>\n  "
+            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a [routerLink]=\"['/dashboard']\" routerLinkActive=\"active\">Dashboard</a>\n      <a [routerLink]=\"['/heroes']\" routerLinkActive=\"active\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
+            styleUrls: ['app/app.component.css'],
+            directives: [router_1.ROUTER_DIRECTIVES],
+            providers: [
+                hero_service_1.HeroService,
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,25 @@
+import { Component }          from '@angular/core';
+import { ROUTER_DIRECTIVES }  from '@angular/router';
 
-import { Component } from '@angular/core';
-
+import { HeroService }        from './hero.service';
 
 @Component({
-    selector: 'my-app',
-    template: `
-  <h1>{{title}}</h1>
+  selector: 'my-app',
 
-  <article>
-      <input type="file" id="imageInput" accept="image/*">
-      <div id="imgdiv"> </div>
-      <div> </div>
-      <div id="svgdiv"> </div>
-  </article>
-  `
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+      <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService,
+  ]
 })
 export class AppComponent {
-    title = 'Converty';
+  title = 'Tour of Heroes';
 }
